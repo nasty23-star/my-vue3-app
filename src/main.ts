c6bee6948a24eb8230cfc92f1c3f6aea544bc15d
Anastasia Yarosh <fysyad@gmail.com>
2025-04-24 14:42:21 +0300

    Подключение определения темы

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+const theme =
+  (localStorage.getItem('theme') as 'dark' | 'light' | null) ||
+  (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+
+document.documentElement.classList.add(theme)
 createApp(App).mount('#app')
